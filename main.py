289448248f8886c8a1e35cc39b1753e6688c2beb
Justinkay0 <14783039+Justinkay0@users.noreply.github.com>
2023-03-31 11:26:24 +0800

    password manager 2 : UI creation

--- main.py
+++ main.py
@@ -4,13 +4,43 @@ import tkinter as tk
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# window initialisation
 window = tk.Tk()
 window.title('Password Manager')
 window.config(padx=20, pady=20)
 
+# password image
 canvas = tk.Canvas(height=200, width=200)
 logo_img = tk.PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
 
+# labels
+web_label = tk.Label(text='Website:', justify='right')
+web_label.grid(row=1, column=0, sticky='E')
+
+username_label = tk.Label(text='Email/Username:', justify='right')
+username_label.grid(row=2, column=0, sticky='E')
+
+password_label = tk.Label(text='Password:', justify='right')
+password_label.grid(row=3, column=0, sticky='E')
+
+# entries
+web_entry = tk.Entry(width=35)
+web_entry.grid(row=1, column=1, columnspan=2, sticky='W')
+
+username_entry = tk.Entry(width=35)
+username_entry.grid(row=2, column=1, columnspan=2,sticky='W')
+
+password_entry = tk.Entry(width=26)
+password_entry.grid(row=3, column=1, columnspan=1, sticky='W')
+
+# buttons
+generate_password_button = tk.Button(text='Generate Password', width=14)
+generate_password_button.grid(row=3, column=2, sticky='W')
+
+add_button = tk.Button(text='Add', width=36)
+add_button.grid(row=4, column=1, columnspan=2, sticky='W')
+# mainloop
 window.mainloop()
+
