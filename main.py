8053627dadfa2130095862200bb5891e707ab8f6
Justinkay0 <14783039+Justinkay0@users.noreply.github.com>
2023-03-31 14:36:05 +0800

    password manager 3 : Saving the password details into file

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import tkinter as tk
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def add_pw():
+    with open(file='data.txt', mode='a') as f:
+        f.write(f"{web_entry.get()} | {username_entry.get()} | {password_entry.get()}\n")
+
+    web_entry.delete(0, tk.END)
+    password_entry.delete(0, tk.END)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # window initialisation
 window = tk.Tk()
 window.title('Password Manager')
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 # password image
 canvas = tk.Canvas(height=200, width=200)
@@ -16,31 +24,34 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 # labels
-web_label = tk.Label(text='Website:', justify='right')
-web_label.grid(row=1, column=0, sticky='E')
+web_label = tk.Label(text='Website:')
+web_label.grid(row=1, column=0)
 
-username_label = tk.Label(text='Email/Username:', justify='right')
-username_label.grid(row=2, column=0, sticky='E')
+username_label = tk.Label(text='Email/Username:')
+username_label.grid(row=2, column=0)
 
-password_label = tk.Label(text='Password:', justify='right')
-password_label.grid(row=3, column=0, sticky='E')
+password_label = tk.Label(text='Password:')
+password_label.grid(row=3, column=0)
 
 # entries
 web_entry = tk.Entry(width=35)
-web_entry.grid(row=1, column=1, columnspan=2, sticky='W')
+web_entry.grid(row=1, column=1, columnspan=2)
+# Place cursor here
+web_entry.focus()
 
 username_entry = tk.Entry(width=35)
-username_entry.grid(row=2, column=1, columnspan=2,sticky='W')
+username_entry.grid(row=2, column=1, columnspan=2)
+username_entry.insert(0, 'CEO@freedom.com')
 
-password_entry = tk.Entry(width=26)
-password_entry.grid(row=3, column=1, columnspan=1, sticky='W')
+password_entry = tk.Entry(width=21)
+password_entry.grid(row=3, column=1, columnspan=1)
 
 # buttons
-generate_password_button = tk.Button(text='Generate Password', width=14)
-generate_password_button.grid(row=3, column=2, sticky='W')
+generate_password_button = tk.Button(text='Generate Password')
+generate_password_button.grid(row=3, column=2)
 
-add_button = tk.Button(text='Add', width=36)
-add_button.grid(row=4, column=1, columnspan=2, sticky='W')
+add_button = tk.Button(text='Add', width=36, command=add_pw)
+add_button.grid(row=4, column=1, columnspan=2)
 # mainloop
 window.mainloop()
 
